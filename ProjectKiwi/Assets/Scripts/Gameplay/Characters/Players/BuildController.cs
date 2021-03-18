c084fea9138889eaf97bd0cec4fddb0cee42e618
Daniel H. G. Mescoloto <dan.gasperoni@gmail.com>
2021-03-18 09:10:21 -0300

    bulding system - placeholder working!

---
+++ ProjectKiwi/Assets/Scripts/Gameplay/Characters/Players/BuildController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildController : MonoBehaviour
+{
+
+    public GameObject placeholder;
+
+   
+    private List<GameObject> _obstructions;
+    private BuildPlaceholder _bp;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        _bp = placeholder.GetComponent <BuildPlaceholder>();
+        _obstructions = new List<GameObject>();
+    }
+
+    void Update()
+    {
+        var _t = transform;
+        var _newPos = _t.position + _t.forward * 2;
+        _newPos.x = Mathf.RoundToInt(_newPos.x);
+        _newPos.z = Mathf.RoundToInt(_newPos.z);
+	    placeholder.transform.position = _newPos;
+    }
+    
+    
+   
+}
